fbdf614de2f7086fa8d36d3507702193e9f4862c
Genjo <insidia93@gmail.com>
2017-05-02 18:13:44 +0300

    Метод $_POST

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+
+//$_GET['id'];
+//var_dump($_GET);
+
+$news = [
+
+    1 =>['title'=>'ЧП!',
+        'text'=>'Проезжая станцию у меня слетела шляпа'],
+    2 =>['title'=>'Все, умерли',
+        'text'=>'Падеж поголовья верблюдов 100%']
+
+
+];
+//$id =(int)$_GET['id'];
+//$article = $news[$id];
+//var_dump($article);
+
+?>
+<!---->
+<!--<h1>--><?php //echo $article['title'] ?><!--</h1>-->
+<!--<p>--><?php //echo $article['text']?><!--</p>-->
